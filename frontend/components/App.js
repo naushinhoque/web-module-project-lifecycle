edc4e9d91aff92ad34318b8d79411511ec89c0b1
naushinhoque <fnmh@yahoo.com>
2023-06-06 18:06:52 -0500

    com

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -14,31 +14,39 @@ export default class App extends React.Component {
     this.setState({ ...this.state, todoNameInput: value })
   }
   resetForm = () => this.setState({ ...this.state, todoNameInput: '' })
-
-  setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message})\
-  
+  setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message})
   postNewTodo = () => {
     axios.post(URL, {name: this.state.todoNameInput})
       .then(res => {
-        this.fetchAllTodos()
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
         this.resetForm()
       })
-      .catch(err => {
-        this.setAxiosResponseError
-      })
+      .catch(this.setAxiosResponseError)
   }
-  onTodoFormSubmit = event => {
-    event.preventDefault()
+  onTodoFormSubmit = evt => {
+    evt.preventDefault()
     this.postNewTodo()
   }
   fetchAllTodos = () => {
     axios.get(URL)
     .then(res => {
-      this.resetForm()
+      this.setState({ ...this.state, todos:res.data.data })
     })
     .catch(err => {
       this.setAxiosResponseError
     })
+  }
+  toggleCompleted = id => evt => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: this.state.todos.map(td => {
+            if (td.id !== id) return td
+            return res.data.data
+          })
+        })
+      })
+      .catch(this.setAxiosResponseError)
   }
   componentDidMount() {
     //fetch all todos from server
@@ -52,7 +60,7 @@ export default class App extends React.Component {
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
+              return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' 🗸' : ''} </div>
             })
           }
         </div>
